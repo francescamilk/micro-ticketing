b1379f9c067d23ed70fdfb8f4470c9ea25a15f89
Francesca Santoriello <f.santoriello@hotmail.com>
2024-02-08 17:10:30 +0100

    connect mongo + k8s depl

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -15,6 +17,17 @@ app.use(json())
 .use(signupRouter)
 .use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000 ~ auth');
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB!')
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000 ~ auth');
+    });
+}
+
+start();
